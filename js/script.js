7497b0ff2332888197884ddde9b48069898fe3c0
Alexandra Vlasceanu <alexandra.vlasceanuuu@gmail.com>
2022-04-12 13:11:48 +0300

    Sticky Navigation Bar

--- js/script.js
+++ js/script.js
@@ -56,6 +56,36 @@ allLinks.forEach(function (link) {
 });
 
 ///////////////////////////////////////////////////////////
+// Sticky navigation
+const sectionHeroEL = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+
+    // if (ent.isIntersecting === true) {
+    //   console.log(ent);
+    //   document.querySelector(".header").classList.remove("sticky");
+    //}
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEL);
+
+///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
